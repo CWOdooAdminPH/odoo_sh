d4bb2775b2deeee20ccfdfac7abbe51508c51833
arne-cw75 <arne.stubbe@codewerk.de>
2022-07-25 16:41:20 +0200

    First Module Update2

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -4,7 +4,6 @@ from odoo import models, fields, api
 
 class my_module(models.Model):
     _name = 'my_module.my_module'
-
     name = fields.Char()
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
