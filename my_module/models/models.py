d62a61fb79fea7d947f39b0ab4caec6080e2c3a0
arne-cw75 <arne.stubbe@codewerk.de>
2022-07-25 17:45:12 +0200

    First Modul Upload6

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -10,6 +10,6 @@ class my_module(models.Model):
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
 
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     self.value2 = float(self.value) / 100
